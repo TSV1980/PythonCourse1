097f0d58e625cf05b55adb09c2d16eb27b5bfaa5
TSV1980 <sergey.v.t.33@gmail.com>
2022-02-21 20:53:31 +0300

    21022022

--- Lesson1/HomeWork/Task2.py
+++ Lesson1/HomeWork/Task2.py
@@ -3,13 +3,13 @@
 
 usersec = int(input('Введите время в секундах:'))
 
-calchour = usersec // 3600
+calchour = usersec // 3600  #расчет часов
 print(f"Количество часов {calchour:02d}")
 
-calminutes = (usersec - (calchour * 3600)) // 60
+calminutes = (usersec - (calchour * 3600)) // 60 # расчте минут
 print(f"Количество минут {calminutes:02d}")
 
-calsecs = usersec - (calchour * 3600) - (calminutes * 60)
+calsecs = usersec - (calchour * 3600) - (calminutes * 60) # расчте секунд
 print(f"Количество секунд {calsecs:02d}")
 
 print(f'Указанные минуты в формате чч:мм:сс - {calchour:02d}:{calminutes:02d}:{calsecs:02d}"')
