f748ea56cc08e5d5d23f67701c303f7879c8d41d
TSV1980 <sergey.v.t.33@gmail.com>
2022-03-01 08:54:38 +0300

    01032022

--- Lesson3/HomeWork/Task3.py
+++ Lesson3/HomeWork/Task3.py
@@ -6,10 +6,7 @@ def my_func3(var1, var2, var3):
 
 
 nums = input('Введите 3 числа через пробел: ').split()
-if (len(nums) >=3):
+if (len(nums) == 3):
     print(f'Результат функции: {my_func3(int(nums[0]),int(nums[1]),int(nums[2]))}')
 else:
     print('Ошибка ввода!')
-
-
-
