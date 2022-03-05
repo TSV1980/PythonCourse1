5388e8376556748551a75f006de6945bc6071b27
TSV1980 <sergey.v.t.33@gmail.com>
2022-03-06 00:03:18 +0300

    04032022

--- Lesson5/HomeTask/Task4.py
+++ Lesson5/HomeTask/Task4.py
@@ -7,3 +7,17 @@
 # При этом английские числительные должны заменяться на русские. 
 # Новый блок строк должен записываться в новый текстовый файл.
 
+translate = {'One':'Один', 'Two':'Два', 'Three':'Три', 'Four':'Четыре'}
+
+f_name_read = 'text_4.txt'
+f_name_write = 'text_4_new.txt'
+
+with open(f_name_read, 'r', encoding='utf-8') as fr:
+    with open(f_name_write, 'w', encoding='utf-8') as fw:
+        lines = fr.readlines()
+        for el in lines:
+            words = el.split()
+            if (len(words) == 3):
+                newstr = translate[words[0]] + ' ' + words[1] + ' ' + words[2] + '\n'
+                fw.write(newstr) 
+                           
