348d63caad7713161d4dd8498467f9a903cd11d9
TSV1980 <sergey.v.t.33@gmail.com>
2022-03-07 19:49:45 +0300

    04032022

--- Lesson5/HomeTask/Task7.py
+++ Lesson5/HomeTask/Task7.py
@@ -13,3 +13,32 @@
 [{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
 Подсказка: использовать менеджер контекста. '''
 
+import json
+
+f_name_read = 'text_7.txt'
+f_name_write = 'text_7_new.json'
+res_dict_firms = {}
+profits = []
+res_list = []
+
+with open(f_name_read, 'r', encoding='utf-8') as fr:
+        lines = fr.readlines()
+        for el in lines:
+            words = el.split()
+            if (len(words) == 4):
+                # words[0] - название 
+                # words[1] - форма собственности
+                # words[2] - выручка
+                # words[3] - издержки
+                delta = int(words[2]) - int(words[3])
+                res_dict_firms.update({words[0] : delta})
+                if (delta > 0):
+                    profits.append(delta)
+
+res_list.append(res_dict_firms)
+res_list.append({'average_profit':sum(profits)/len(profits)})
+
+print(res_list)
+
+with open(f_name_write, "w", encoding='utf-8') as write_f:
+    json.dump(res_list, write_f)
